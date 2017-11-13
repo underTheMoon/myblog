13bbbff56f7081f0264bb35f028e5273fdb7d554
underTheMoon <373863770@qq.com>
2017-11-13 12:32:10 +0800

    1

--- 新建文件夹/public/admin/header.js
+++
@@ -1,9 +0,0 @@
-var txt='<header>\
-<h1>网站后台管理系统</h1>\
-<p>\
-<a href="./admin.html"><span class="icon home"></span>系统首页</a>\
-<a href="/users/logout"><span class="icon quit"></span>安全退出</a>\
-</p>\
-</header>'
-
-document.write(txt);
